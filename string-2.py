43b6c10fc95afabbc0b4d1589c236a2ee2091a86
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-03-17 12:15:27 +0530

    Add files via upload

---
+++ string-2.py
@@ -0,0 +1,6 @@
+# string using name
+characters = "my_name_is_yogeesh"
+print(characters[0])                  # 1st value
+print(characters[-1])                 # last value
+print(characters[4])                  # 4th value
+print(characters[11:18])              # 11th to 18th value
